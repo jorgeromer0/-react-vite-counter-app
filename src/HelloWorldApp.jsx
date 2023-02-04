47d4861f1d0c959072dfb26b6302aaf41009be42
Jorge <jorgerom@hotmail.es>
2023-02-04 18:12:29 +0100

    v1

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,14 @@
+//  function App() {
+//     return (
+//       <div>Hola Mundo</div>
+//     )
+//   }
+  
+//   export default App;
+import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
